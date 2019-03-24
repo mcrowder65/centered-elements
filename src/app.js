8ec89d0091bb73905686e94ee7e35da6930e3751
Matt Crowder <mcrowder65@gmail.com>
2019-03-24 19:00:57 -0400

    30 second video

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Grid} from '@material-ui/core';
+
+function App() {
+  return (
+    <Grid container justify="center" alignItems="center">
+      <Grid item>
+      Hello world
+      </Grid>
+    </Grid>
+  )
+}
+
+export default App;
